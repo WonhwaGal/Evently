144ac2bcc66a05c36ac815bb3832f0dbe823cfe2
WonhwaGal <110241714+WonhwaGal@users.noreply.github.com>
2025-05-18 20:45:48 +0300

    add new api - Gateway (reverse proxy)

---
+++ src/API/Evently.Gateway/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Evently.Gateway.Middleware;
+
+public static class MiddlewareExtensions
+{
+    internal static IApplicationBuilder UseLogContextTraceLogging(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<LogContextTraceLoggingMiddleware>();
+
+        return app;
+    }
+}
