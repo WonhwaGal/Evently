d29680bce2796436bdbdcd0f78a5ec305e2926f9
WonhwaGal <110241714+WonhwaGal@users.noreply.github.com>
2025-07-20 18:03:56 +0300

    ticket configuration changed

--- src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/Migrations/20250720145237_Modify_Tickets.Designer.cs
+++ src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/Migrations/20250720145237_Modify_Tickets.Designer.cs
@@ -4,6 +4,7 @@ using Evently.Modules.Attendance.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Evently.Modules.Attendance.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AttendanceDbContext))]
-    partial class AttendanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250720145237_Modify_Tickets")]
+    partial class Modify_Tickets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/Migrations/20250720145237_Modify_Tickets.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evently.Modules.Attendance.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modify_Tickets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "code",
+                schema: "attendance",
+                table: "tickets",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "code",
+                schema: "attendance",
+                table: "tickets",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
